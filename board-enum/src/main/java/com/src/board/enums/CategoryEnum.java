7ac059a2b344e431b4604055265cbc6a262ae731
Nagaraju M <nagaraju51makkena@gmail.com>
2015-07-06 17:57:00 +0530

    Added Constructor for CategoryStatus

--- board-enum/src/main/java/com/src/board/enums/CategoryEnum.java
+++ board-enum/src/main/java/com/src/board/enums/CategoryEnum.java
@@ -2,10 +2,36 @@ package com.src.board.enums;
 
 public enum CategoryEnum {
 
-	UNCATEGORIZED,
-	BUG,
-	DOCUMENTATION,
-	ENHANCEMENT,
-	FEATURE
+	UNCATEGORIZED("1"),
+	BUG("2"),
+	DOCUMENTATION("3"),
+	ENHANCEMENT("4"),
+	FEATURE("5");
+	
+	private String status;
+
+	private CategoryEnum(String status) {
+		this.status = status;
+	}
+	
+	public String getStatus() {
+		return status;
+	}	
+	
+	public static CategoryEnum fromString(String text) {
+	    if (text != null) {
+	      for (CategoryEnum b : CategoryEnum.values()) {
+	        if (text.equalsIgnoreCase(b.status)) {
+	          return b;
+	        }
+	      }
+	    }
+	    return null;
+	  }
+	
+	@Override
+	public String toString() {		
+		return getStatus();
+	}
 	
 }
