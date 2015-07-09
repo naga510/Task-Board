9f55a2aa094c4fcbec8e69c2b0875079871ea043
Nagaraju M <nagaraju51makkena@gmail.com>
2015-07-09 16:36:53 +0530

    Added Named Query for finding Items by BoardId

--- board-domain/src/main/java/com/src/board/domain/Item.java
+++ board-domain/src/main/java/com/src/board/domain/Item.java
@@ -1,28 +1,25 @@
 package com.src.board.domain;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 
 @Entity
 @Table(name="ITEMS")
+@NamedQuery(name="findItemsByBoardId", query="from Item item where item.board.boardId = :boardId")
 public class Item implements Serializable{
 
 	/**
